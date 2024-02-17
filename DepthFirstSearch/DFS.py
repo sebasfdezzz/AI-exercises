234d14632788cc860446f63e247f9acaeff6c935
Sebastian Fernandez Lopez <101682500+sebasfdezzz@users.noreply.github.com>
2024-02-17 14:17:18 -0600

    DFS py

--- DepthFirstSearch/DFS.py
+++ DepthFirstSearch/DFS.py
@@ -1,53 +1,129 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+import random
 
 G = nx.Graph() #DiGrpah es direccionada
 G.add_nodes_from(['Cozumel','Ciudad del Carmen','Angel Albino Corzo','Minatitlan','Veracruz','Acapulco','Puebla','CDMX','Morelia','Uruapan','San Luis Potosi','Tampico','Piedras Negras','Culiacan','Abraham Gonzalez','Nogales','Guaymas','Loreto','Manuel Marquez de Leon','Los Cabos'])
 print(G.nodes())
 
 G.add_edges_from([
-('Cozumel', 'Tampico', {'weight': 1}),
-('Ciudad del Carmen', 'Cozumel', {'weight': 1}),
-('Angel Albino Corzo', 'Ciudad del Carmen', {'weight': 1}),
-('Angel Albino Corzo', 'Minatitlan', {'weight': 1}),
+('Cozumel', 'Tampico', {'weight': 17}),
+('Ciudad del Carmen', 'Cozumel', {'weight': 15}),
+('Angel Albino Corzo', 'Ciudad del Carmen', {'weight': 11}),
+('Angel Albino Corzo', 'Minatitlan', {'weight': 15}),
 ('Angel Albino Corzo', 'Acapulco', {'weight': 1}),
-('Minatitlan', 'Ciudad del Carmen', {'weight': 1}),
-('Minatitlan', 'Veracruz', {'weight': 1}),
-('Acapulco', 'Morelia', {'weight': 1}),
-('Acapulco', 'Uruapan', {'weight': 1}),
-('Puebla', 'Veracruz', {'weight': 1}),
-('Puebla', 'Acapulco', {'weight': 1}),
-('CDMX', 'Acapulco', {'weight': 1}),
-('Morelia', 'CDMX', {'weight': 1}),
-('Morelia', 'San Luis Potosi', {'weight': 1}),
-('Uruapan', 'Culiacan', {'weight': 1}),
-('San Luis Potosi', 'Abraham Gonzalez', {'weight': 1}),
-('Tampico', 'Puebla', {'weight': 1}),
-('Tampico', 'CDMX', {'weight': 1}),
-('Tampico', 'San Luis Potosi', {'weight': 1}),
-('Piedras Negras', 'San Luis Potosi', {'weight': 1}),
-('Piedras Negras', 'Tampico', {'weight': 1}),
-('Culiacan', 'Piedras Negras', {'weight': 1}),
-('Culiacan', 'Guaymas', {'weight': 1}),
-('Abraham Gonzalez', 'Piedras Negras', {'weight': 1}),
-('Nogales', 'Abraham Gonzalez', {'weight': 1}),
-('Guaymas', 'Abraham Gonzalez', {'weight': 1}),
-('Guaymas', 'Nogales', {'weight': 1}),
-('Guaymas', 'Loreto', {'weight': 1}),
-('Loreto', 'Manuel Marquez de Leon', {'weight': 1}),
-('Manuel Marquez de Leon', 'Culiacan', {'weight': 1}),
-('Manuel Marquez de Leon', 'Los Cabos', {'weight': 1}),
+('Minatitlan', 'Ciudad del Carmen', {'weight': 16}),
+('Minatitlan', 'Veracruz', {'weight': 18}),
+('Acapulco', 'Morelia', {'weight': 14}),
+('Acapulco', 'Uruapan', {'weight': 14}),
+('Puebla', 'Veracruz', {'weight': 12}),
+('Puebla', 'Acapulco', {'weight': 19}),
+('CDMX', 'Acapulco', {'weight': 4}),
+('Morelia', 'CDMX', {'weight': 15}),
+('Morelia', 'San Luis Potosi', {'weight': 11}),
+('Uruapan', 'Culiacan', {'weight': 15}),
+('San Luis Potosi', 'Abraham Gonzalez', {'weight': 15}),
+('Tampico', 'Puebla', {'weight': 7}),
+('Tampico', 'CDMX', {'weight': 8}),
+('Tampico', 'San Luis Potosi', {'weight': 11}),
+('Piedras Negras', 'San Luis Potosi', {'weight': 11}),
+('Piedras Negras', 'Tampico', {'weight': 16}),
+('Culiacan', 'Piedras Negras', {'weight': 8}),
+('Culiacan', 'Guaymas', {'weight': 16}),
+('Abraham Gonzalez', 'Piedras Negras', {'weight': 15}),
+('Nogales', 'Abraham Gonzalez', {'weight': 6}),
+('Guaymas', 'Abraham Gonzalez', {'weight': 18}),
+('Guaymas', 'Nogales', {'weight': 5}),
+('Guaymas', 'Loreto', {'weight': 13}),
+('Loreto', 'Manuel Marquez de Leon', {'weight': 15}),
+('Manuel Marquez de Leon', 'Culiacan', {'weight': 14}),
+('Manuel Marquez de Leon', 'Los Cabos', {'weight': 10}),
 ])
 
-def DFS(orig, dest):
-  if orig == dest:
-    return dest
-  
-  checked = [orig]
-  resting = G.nodes()
-  resting.remove(orig)
+def DFS(node, dest, visited=None):
+    if visited is None:
+        visited = set()
 
-  while resting:
+    visited.add(node)
+
+    if node == dest:
+        return [node]
+
+    if G.neighbors(node):
+        for child in G.neighbors(node):
+            if child not in visited:
+                path = DFS(child, dest, visited)
+                if path:
+                    return [node] + path
+    return None
+
+
+def DFS_limited(node, dest, limit, depth=0, visited=None):
+    if visited is None:
+        visited = set()
+
+    visited.add(node)
+
+    if node == dest:
+        return [node]
+
+    if depth >= limit:
+        return None  # Depth limit exceeded
+
+    if node in G:  # Check if node exists in the graph
+        for child in G[node]:
+            weight = G[node][child]['weight']  # Access weight of the edge
+            if child not in visited and weight <= limit - depth:
+                path = DFS_limited(child, dest, limit, depth + weight, visited)
+                if path:
+                    return [node] + path
+    return None
+
+
+def DFS_rnd(node, dest, visited=None):
+    if visited is None:
+        visited = set()
+
+    visited.add(node)
+
+    if node == dest:
+        return [node]
+
+    children = [child for child in G.neighbors(node) if child not in visited]
+    if children:
+        random_child = random.choice(children)
+        path = DFS_rnd(random_child, dest, visited)
+        if path:
+            return [node] + path
+    return None
+
+
+def DFS_limited_rnd(node, dest, limit, depth=0, visited=None):
+    if visited is None:
+        visited = set()
+
+    visited.add(node)
+
+    if node == dest:
+        return [node]
+
+    if depth >= limit:
+        return None
+    children = [child for child in G.neighbors(node) if child not in visited]
+    if children:
+            child = random.choice(children)
+            weight = G[node][child]['weight']
+            if child not in visited and weight <= limit - depth:
+                path = DFS_limited(child, dest, limit, depth + weight, visited)
+                if path:
+                    return [node] + path
+    return None
+
+
+print(DFS('Cozumel','Acapulco'))
+print(DFS_rnd('Cozumel','Acapulco'))
+print(DFS_limited('Cozumel','Acapulco',60))
+print(DFS_limited_rnd('Cozumel','Acapulco',900))
     
 
 
