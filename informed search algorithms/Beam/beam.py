18734e6f389b055baada8c74174b597b9448aed4
Sebastian Fernandez Lopez <101682500+sebasfdezzz@users.noreply.github.com>
2024-03-17 18:31:22 -0600

    beam py

---
+++ informed search algorithms/Beam/beam.py
@@ -0,0 +1,76 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+G = nx.Graph() #DiGrpah es direccionada
+G.add_nodes_from(['Cozumel','Ciudad del Carmen','Angel Albino Corzo','Minatitlan','Veracruz','Acapulco','Puebla','CDMX','Morelia','Uruapan','San Luis Potosi','Tampico','Piedras Negras','Culiacan','Abraham Gonzalez','Nogales','Guaymas','Loreto','Manuel Marquez de Leon','Los Cabos'])
+print(G.nodes())
+
+G.add_edges_from([
+('Cozumel', 'Tampico', {'weight': 17}),
+('Ciudad del Carmen', 'Cozumel', {'weight': 15}),
+('Angel Albino Corzo', 'Ciudad del Carmen', {'weight': 11}),
+('Angel Albino Corzo', 'Minatitlan', {'weight': 15}),
+('Angel Albino Corzo', 'Acapulco', {'weight': 1}),
+('Minatitlan', 'Ciudad del Carmen', {'weight': 16}),
+('Minatitlan', 'Veracruz', {'weight': 18}),
+('Acapulco', 'Morelia', {'weight': 14}),
+('Acapulco', 'Uruapan', {'weight': 14}),
+('Puebla', 'Veracruz', {'weight': 12}),
+('Puebla', 'Acapulco', {'weight': 19}),
+('CDMX', 'Acapulco', {'weight': 4}),
+('Morelia', 'CDMX', {'weight': 15}),
+('Morelia', 'San Luis Potosi', {'weight': 11}),
+('Uruapan', 'Culiacan', {'weight': 15}),
+('San Luis Potosi', 'Abraham Gonzalez', {'weight': 15}),
+('Tampico', 'Puebla', {'weight': 7}),
+('Tampico', 'CDMX', {'weight': 8}),
+('Tampico', 'San Luis Potosi', {'weight': 11}),
+('Piedras Negras', 'San Luis Potosi', {'weight': 11}),
+('Piedras Negras', 'Tampico', {'weight': 16}),
+('Culiacan', 'Piedras Negras', {'weight': 8}),
+('Culiacan', 'Guaymas', {'weight': 16}),
+('Abraham Gonzalez', 'Piedras Negras', {'weight': 15}),
+('Nogales', 'Abraham Gonzalez', {'weight': 6}),
+('Guaymas', 'Abraham Gonzalez', {'weight': 18}),
+('Guaymas', 'Nogales', {'weight': 5}),
+('Guaymas', 'Loreto', {'weight': 13}),
+('Loreto', 'Manuel Marquez de Leon', {'weight': 15}),
+('Manuel Marquez de Leon', 'Culiacan', {'weight': 14}),
+('Manuel Marquez de Leon', 'Los Cabos', {'weight': 10}),
+])
+
+def create_heuristic_table(goal):
+    h = {}
+    for node in G.nodes():
+        h[node] = random.randint(20, 50)
+    h[goal] = 0
+    return h
+
+
+def beam_search(origin, destination, beam_width=3):
+    h = create_heuristic_table(destination)
+
+    beam = [(origin, [origin], 0)]
+
+    while beam:
+        new_beam = []
+
+        for node, path, cost in beam:
+            if node == destination:
+                return path, cost
+
+            for neighbor in G.neighbors(node):
+                new_path = path + [neighbor]
+                new_cost = cost + G.get_edge_data(node, neighbor).get('weight') + h[neighbor]
+
+                new_beam.append((neighbor, new_path, new_cost))
+
+        new_beam.sort(key=lambda x: x[2])
+        beam = new_beam[:beam_width]
+
+    return "Path not found"
+
+
+origin = input("Origin: ")
+destination = input("Destination: ")
+print(beam_search(origin, destination))
